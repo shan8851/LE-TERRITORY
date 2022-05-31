9fd6d8423f44d3ee5e15391ec2ed228b7b41b220
shan5742 <asamshan456@gmail.com>
2022-05-31 21:36:45 +0100

    setting map

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import {maps} from './data/maps'
+import { useState } from 'react';
+import { maps } from './data/maps';
 
 export const App = () => {
+  const [map, setMap] = useState(null);
+
   return (
-    <div style={{display: 'flex', flexDirection: 'column'}}>
-     <div style={{display: 'flex', flexWrap: 'wrap'}}>
-      {maps.map(item => (
-        <p style={{margin: 5, cursor: 'pointer'}} key={item.title}>{item.title}</p>
-      ))}
-    </div>
+    <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+      <div style={{ display: 'flex', flexWrap: 'wrap', marginBottom: 20 }}>
+        {maps.map((item) => (
+          <div key={item.title}>
+            <p
+              onClick={() => setMap(item.mapUrl)}
+              style={{ margin: 5, cursor: 'pointer' }}
+            >
+              {item.title}
+            </p>
+          </div>
+        ))}
+      </div>
+      {map && map}
     </div>
   );
-}
+};
 
 export default App;
